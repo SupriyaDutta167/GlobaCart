ac70ed1102d235df530be710609fd7769afb3f98
SupriyaDutta167 <151374699+SupriyaDutta167@users.noreply.github.com>
2025-09-24 14:11:58 +0530

    Initial commit: Spring Boot GlobaCart project

---
+++ backend/src/main/java/com/globacart/GlobaCartApplication.java
@@ -0,0 +1,13 @@
+package com.globacart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaCartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/globacart/GlobaCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globacart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobaCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
